08ef4aa09852b41bf4c819da81d3007c61f7d64f
Kristina Calumpong <kcalumpong@gmail.com>
2019-10-23 11:11:02 -0700

    initial files

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+        <div>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Flag Memory Game</h1>
+                    <p className="lead">Click on a flag to earn points, but don't click on any more than once.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="nav">
+                <h1>Flag Memory Game</h1>
+                <p>Click on a flag to earn points, but don't click on any more than once</p>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
