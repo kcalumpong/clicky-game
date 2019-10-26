74376b021d07e148d28a83c485b44fd6c6b63c98
Kristina Calumpong <kcalumpong@gmail.com>
2019-10-26 00:39:42 -0700

    Updates

---
+++ precache-manifest.f663e22b5d2d5af5ae0458071b340f67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc847da53ebadcb2fd74fc622b1c2ea",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "bede2ca4256a57380cc6",
+    "url": "/clicky-game/static/css/main.fc5a3d1f.chunk.css"
+  },
+  {
+    "revision": "7fc024a4fe7cc60fae2a",
+    "url": "/clicky-game/static/js/2.b19a2de5.chunk.js"
+  },
+  {
+    "revision": "bede2ca4256a57380cc6",
+    "url": "/clicky-game/static/js/main.73ec654c.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
